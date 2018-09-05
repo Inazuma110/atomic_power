3aac708c5d19277fc6b0103cb9210363db552beb
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-09-05 14:46:18 +0900

    add: writeVimSnippet

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const rls = require('readline-sync');
 const cson = require('cson');
 const exec = require('child_process').exec;
+const fs = require('fs');
 
-exec('node_modules/cson/bin/cson2json snippet.cson > snippet.json', (err, stdout, stderr) => {
-  if (err) { console.log(err); }
-  console.log(stdout);
-});
 
+const csonToJson = () => {
+  exec('node_modules/cson/bin/cson2json snippet.cson > snippet.json', (err, stdout, stderr) => {
+    if (err) { console.log(err); }
+    else console.log('Cson snippet trans json.');
+
+    let jsonSnippets;
+    try {
+      jsonSnippets = JSON.parse(fs.readFileSync('./snippet.json',  'utf-8'));
+    } catch (e) {
+      throw new Error('Error! check your cson file.')
+    }
+    writeVimSnippet(jsonSnippets);
+  });
+}
+
+const writeVimSnippet = (jsonSnippets) => {
+  // console.log(jsonSnippets);
+  console.log('Please enter extension. ex) cpp');
+  console.log("If you don't know extends, please check https://github.com/atom/autocomplete-plus/wiki/Autocomplete-Providers");
+
+  for (let lang in jsonSnippets) {
+    const extension = rls.question(`What is extension of ${lang}?: `);
+    for(let name in jsonSnippets[lang]){
+      for(let elem in jsonSnippets[lang][name]){
+        // console.log(jsonSnippets[lang][name][elem]);
+      }
+    }
+  }
+}
+
+(async() => {
+  await csonToJson();
+})();
