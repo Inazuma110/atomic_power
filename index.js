f683d3ed2c444767ae280bb9e078a65f55bb06a7
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-09-06 17:23:28 +0900

    add: parseBody

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cson = require('cson');
 const exec = require('child_process').exec;
 const fs = require('fs');
 
-const writeFile = (path, data) => {
-  fs.appendFileSync(path, data, (err) => {
+const appendFile = (path, data) => {
+  fs.appendFile(path, data, (err) => {
     if(err) throw error;
   });
 }
@@ -27,25 +27,59 @@ const csonToJson = () => {
 const writeVimSnippet = (jsonSnippets) => {
   console.log(jsonSnippets);
   console.log('Please enter extension. ex) cpp');
-  console.log("If you don't know extends, please check https://github.com/atom/autocomplete-plus/wiki/Autocomplete-Providers");
+  console.log("If you don't know extension,");
+  console.log("please check https://github.com/atom/autocomplete-plus/wiki/Autocomplete-Providers");
 
   for (let lang in jsonSnippets) {
     const extension = rls.question(`What is extension of ${lang}?: `);
     for(let name in jsonSnippets[lang]){
-      for(let elem in jsonSnippets[lang][name]){
-        const fileName = `./${extension}.snip`;
-        writeFile(fileName, `snippet	${name}` + "\n");
-        writeFile(fileName, `abbr	${jsonSnippets[lang][name]['prefix']}` + "\n");
-        writeFile(fileName, `	${jsonSnippets[lang][name]['body']}` + "\n");
-        // console.log(jsonSnippets[lang][name][elem]);
+      let fileName = `./${extension}.snip`;
+      let body;
+      try {
+        body = parseBody(jsonSnippets[lang][name]['body']);
+      } catch (e) {
+        console.log("Cannot find " + jsonSnippets[lang][name]['prefix'] + "'s body.");
+        console.log("this is skipeed.");
+        continue;
       }
+      appendFile(fileName, `snippet	${name}` + "\n");
+      appendFile(fileName, `abbr	${jsonSnippets[lang][name]['prefix']}` + "\n");
+      appendFile(fileName, "\t" + body + "\n\n");
+      // console.log(jsonSnippets[lang][name][elem]);
     }
   }
 }
 
-// const parseBody = () => {
+const parseBody = (body) => {
+  let indent = '(  )+?';
+  indent = new RegExp(indent, 'g');
+  body = body.replace(indent, '\t');
 
-// }
+  let newest = '1';
+  while (true) {
+    let isFirst = true;
+    let search = '(\\$)(\\{)' + newest + '(:)(.+?)(\\})';
+    let searchReg = new RegExp(search);
+    let matchIndex = body.match(searchReg);
+    if(matchIndex == null) break;
+    let firstIndex;
+    while (true) {
+      let index = body.match(searchReg);
+      if(index == null) break;
+      if(isFirst){
+        isFirst = false;
+        firstIndex = index;
+        body = body.replace(searchReg, '$1$2@' + newest + '$3$4$5');
+      }
+      else body = body.replace(searchReg, '$1' + newest);
+    }
+    body = body.slice(0, firstIndex['index']+2) + body.slice(firstIndex['index']+3);
+    newest = (Number(newest)+1) + '';
+  }
+  return body;
+}
+
+
 
 (async() => {
   await csonToJson();
