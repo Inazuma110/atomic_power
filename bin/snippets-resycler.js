2bf23ec1ed23fd5262a3d66669cd6783dc441658
Inazuma110 <c011703534@edu.teu.ac.jp>
2018-09-09 02:47:34 +0900

    add: main func

--- bin/snippets-resycler.js
+++ bin/snippets-resycler.js
@@ -3,12 +3,12 @@
 const fs = require('fs');
 const Lib = require('../index');
 
-(async() => {
+const main = async() => {
   const lib = new Lib();
   await lib.csonToJson();
   let snippets = await fs.readFileSync('./snippet.json',  'utf-8');
   snippets = await JSON.parse(snippets);
   lib.writeVimSnippet(snippets);
-})();
+}
 
-
+main();
